ae3e997bb84fb6323ad8d83c8d95a8c8a46b3eb6
Jakub Niewczas <niewczas.jakub@gmail.com>
2015-07-16 11:11:32 +0200

    proper di in nl2br filter

--- source/assets/app/app.js
+++ source/assets/app/app.js
@@ -4,11 +4,11 @@ angular.module('jakubniewczas', [
 ]);
 
 angular.module('jakubniewczas')
-  .filter('nl2br', function($sce) {
+  .filter('nl2br', ['$sce', function($sce) {
     return function(msg, is_xhtml) { 
       var is_xhtml = is_xhtml || true;
       var breakTag = (is_xhtml) ? '<br />' : '<br>';
       var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
       return $sce.trustAsHtml(msg);
     }
-  });
+  }]);
