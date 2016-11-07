b1817b2a0521b878322207e2ace136d679a2c5bd
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-11-07 21:32:00 +0100

    change file descriptions

--- source/assets/app/application.js
+++ source/assets/app/application.js
@@ -1,7 +1,6 @@
-// 
+//
 // This file is bootstrap for whole app.
-// it loads components javascripts
-// 
+//
 //= require angular
 //= require angular-translate
 //= require angular-translate-loader-static-files
