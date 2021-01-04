4753cbffa6b4992bed734727e7db75bad962ffa7
Jakub Niewczas <niewczas.jakub@gmail.com>
2021-01-04 22:01:03 +0900

    typo

--- scripts/deployment/deploy-to-gh-pages-docker.sh
+++ scripts/deployment/deploy-to-gh-pages-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ -n "$REBUILD" ] || [ $(docker images | grep 'jakubniewczas-com-dev' | wc -l) -eq 0 ] ; then
-  docker build -t jakubniewczas-com-dev -f ./scripts/DevEnvDockerfile .
+if [ -n "$REBUILD" ] || [ $(docker images | grep 'jakubniewczas-pl-dev' | wc -l) -eq 0 ] ; then
+  docker build -t jakubniewczas-pl-dev -f ./scripts/DevEnvDockerfile .
 fi
 
 docker run -it --rm \
   -v "$PWD":/usr/src/app \
   -v "$HOME/.ssh":/root/.ssh \
   -p 3000:3000 \
-  jakubniewczas-com-dev \
+  jakubniewczas-pl-dev \
   sh -c "bundle ; sh ./scripts/deployment/deploy-to-gh-pages.sh"
--- scripts/development/run-env-docker.sh
+++ scripts/development/run-env-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ -n "$REBUILD" ] || [ $(docker images | grep 'jakubniewczas-com-dev' | wc -l) -eq 0 ] ; then
-  docker build -t jakubniewczas-com-dev -f ./scripts/DevEnvDockerfile .
+if [ -n "$REBUILD" ] || [ $(docker images | grep 'jakubniewczas-pl-dev' | wc -l) -eq 0 ] ; then
+  docker build -t jakubniewczas-pl-dev -f ./scripts/DevEnvDockerfile .
 fi
 
 docker run -it --rm \
   -v "$PWD":/usr/src/app \
   -p 3000:3000 \
-  jakubniewczas-com-dev \
+  jakubniewczas-pl-dev \
   middleman -p 3000
