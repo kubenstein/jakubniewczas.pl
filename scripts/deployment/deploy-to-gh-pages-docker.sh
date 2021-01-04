b64b6a54ad239ea17ed8dbc9df8376c2b8a0b97b
Jakub Niewczas <niewczas.jakub@gmail.com>
2021-01-04 22:11:02 +0900

    dont share build folder with docker host during deployment

--- scripts/deployment/deploy-to-gh-pages-docker.sh
+++ scripts/deployment/deploy-to-gh-pages-docker.sh
@@ -6,6 +6,7 @@ fi
 docker run -it --rm \
   -v "$PWD":/usr/src/app \
   -v "$HOME/.ssh":/root/.ssh \
+  -v /usr/src/app/build \
   -p 3000:3000 \
   jakubniewczas-pl-dev \
   sh -c "bundle ; sh ./scripts/deployment/deploy-to-gh-pages.sh"
